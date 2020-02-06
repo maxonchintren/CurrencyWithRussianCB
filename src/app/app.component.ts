1ea8e2abc0f377b60048296ed8aa223c65f3f135
maxonchintren <maxonchintren@mail.ru>
2020-02-06 20:05:26 +0300

    Added styles and news section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ import { JsonpClientBackend } from '@angular/common/http';
 export class AppComponent implements OnInit{
   title = 'currency';
   public todaysCurrency = [];
+  public inputValue = ''
   public error = false;
+  public news;
 
   constructor(private _currencyService : CurrencyServiceService) {}
 
@@ -19,13 +21,14 @@ export class AppComponent implements OnInit{
     let date = <HTMLInputElement>document.getElementById('dates');
     date.valueAsDate = new Date();
     date.max = date.value
-    console.log(date.value)
 
     this._currencyService.getTodaysCurrency().subscribe((data) => {
       for (let val in data['Valute']) {
         this.todaysCurrency.push((data['Valute'][val]));
       }
     });
+
+    this._currencyService.getNews().subscribe((data) => this.news = data["articles"].splice(0,5))
   }
 
   currencyForDate() {
@@ -40,6 +43,18 @@ export class AppComponent implements OnInit{
     );
   }
   
+  valuteSearch() {
+    let input = <HTMLInputElement>document.getElementById('search')
+    this. inputValue = input.value.toLowerCase();
+    console.log(input.value)
+    console.log(this.news)
+  }
   
-  
+  hasImg(article) {
+    if (article.urlToImage) {
+      return article.urlToImage;
+    } else {
+      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRwSVnSwVQ2xdVCsWMSya3NIGfeRqAeVvbhrGGyxnufLoA_LgIM';
+    }
+  }
 }
--- src/app/currency-service.service.ts
+++ src/app/currency-service.service.ts
@@ -18,4 +18,8 @@ export class CurrencyServiceService {
     let date = dateInput.value.split('-').join('\/\\');
     return this._http.get(`\/\/www.cbr-xml-daily.ru\/archive\/${date}\/daily_json.js`)
   }
+
+  getNews() {
+    return this._http.get('https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=fe0548f6b21349c195866c60479dc3f3');
+  }
 }
